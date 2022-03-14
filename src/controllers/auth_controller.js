c67b7ed111631a798a68e8c144a4216ace404611
unknown <projetofapesq>
2022-03-13 23:15:44 -0300

    subindo tudo do fds

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -6,6 +6,7 @@ const Users = require("../models/users_model");
 module.exports ={
     registerUsers: async (req, res, next) =>{
         try{
+            console.log(req.body)
             const users = new Users(req.body);
             const result = await users.save();
             const {senha, ...user} = result.toObject();
@@ -14,18 +15,38 @@ module.exports ={
 
             res.send({user, token});
         }catch(err){
+            console.log(err.message);
+            if(err.name === "ValidationError"){
+                res.status(422).json({erro: err.message});
+                return;
+            }
+            next(err);
+        }
+    },
+    loginUsers: async (req, res, next) =>{
+        const [, hash] = req.headers.authorization.split(" ");
+        const [email, senha] = Buffer.from(hash, "base64").toString().split(":");
+
+        try{
+            const user = await Users.findOne({email, senha});
+
+            if(!user){
+                res.status(401).json({msg: "Usuario não Existe...."});
+            }
+
+            const token = Auth.Sign({user: user.id});
+
+            res.send({user,token});
+        }catch(error){
             console.log(error.message);
-            if(error.name === "ValidationError"){
-                res.status(422).json({erro: error.message});
+            if(error instanceof mongoose.CastError){
+                next(res.status(400).json({mensagem: "Usuario não existe..."}));
                 return;
             }
             next(error);
         }
     },
-    loginUsers: async (req, res, next) =>{
-
-    },
     userOn: async (req,res,next) =>{
-
+        res.send(req.auth);
     }
 }
