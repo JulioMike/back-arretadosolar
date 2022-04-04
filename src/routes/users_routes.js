98586c074be69015b8f6ad2e64fbc8869c81d5f8
JulioMike <julio.oliveira@ee.ufcg.edu.br>
2022-04-04 08:49:50 -0300

    enredeco mudou

--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const users_controller = require('../controllers/user_controller');
 const midd = require('../middleware/authMiddleware');
 
-router.get('/users', midd.authMiddleware, users_controller.getAllUsers);
+router.get('/users', users_controller.getAllUsers);
 router.get('/users/:id', midd.authMiddleware, users_controller.findUsersById);
 router.put('/users/:id', midd.authMiddleware, users_controller.updateUsers);
 router.delete('/users/:id', midd.authMiddleware, users_controller.deleteUsers);
